a72843414b3cc8576d4d944b96eb8df24f207fbb
ClaudeAzure <92241395+ClaudeAzure@users.noreply.github.com>
2022-06-06 14:56:25 +0700

    Update GameForeground.java

--- src/component/GameForeground.java
+++ src/component/GameForeground.java
@@ -1,8 +1,6 @@
 package component;
 
 import java.awt.Graphics;
-
-
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +15,7 @@ import util.GameUtil;
  * @author BUILD SUCCESSFUL
  */
 public class GameForeground {
-    private final List<Cloud> clouds;
+    private final List<Cloud> clouds; // Container for clouds
     private final BufferedImage[] cloudImages;
     
     //import clouds( two pictures ) into arraylist , cloudimage will select randomly
@@ -36,16 +34,17 @@ public class GameForeground {
         }
     }
     
+    // Control of cloud
     private void cloudBornLogic() {
-      // if the number of cloud less than the rule ( 7 clouds on the screen ) , then continue import til it still satisfied the rule
+            // if the number of cloud less than the rule ( 7 clouds on the screen ) , then continue import til it still satisfied the rule
             if (clouds.size() < Constant.MAX_CLOUD_NUMBER) {
                 try {
                     if (GameUtil.isInProbability(Constant.CLOUD_BORN_PERCENT, 100)) { // using probability class 
                         int index = GameUtil.getRandomNumber(0, Constant.CLOUD_IMAGE_NUMBER); // select randomly cloud 
 
-                     //x is the width of frame it's means that the cloud will run from the right of screen 
+                        //x is the width of frame it's means that the cloud will run from the right of screen 
                         int cloudX = Constant.FRAME_WIDTH; 
-                      // y randomly from the top screen to the 1/3  frame height
+                        // y randomly from the top screen to the 1/3  frame height
                         int cloudY = GameUtil.getRandomNumber(Constant.TOP_BAR_HEIGHT, Constant.FRAME_HEIGHT / 3);
 
                         Cloud cloud = new Cloud(cloudImages[index], cloudX, cloudY);
