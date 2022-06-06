b584fbe6d860744f7050dfb86da28f6155f4f486
ClaudeAzure <92241395+ClaudeAzure@users.noreply.github.com>
2022-06-06 14:48:53 +0700

    Update MovingPipe.java

--- src/component/MovingPipe.java
+++ src/component/MovingPipe.java
@@ -2,7 +2,6 @@ package component;
 
 import java.awt.Graphics;
 
-
 import util.Constant;
 
 /**
@@ -13,16 +12,18 @@ import util.Constant;
 
 public class MovingPipe extends Pipe {
 
-    private int deltaY;
+    private int deltaY; // Coordinates for moving water pipes
     public static final int MAX_DELTA = 50; // Maximum travel distance
     private int direction;
     public static final int DIRECTION_UP = 0;
     public static final int DIRECTION_DOWN = 1;
 
+    // Constructors
     public MovingPipe() {
         super();
     }
 
+    // Attribute of each water pipe parameter 
     public void setAttribute(int x, int y, int height, int type, boolean visible) {
         super.setAttribute(x, y, height, type, visible);
         deltaY = 0;
@@ -46,12 +47,14 @@ public class MovingPipe extends Pipe {
                 break;
 
         }
+        //The water pipe stops moving after the bird dies
         if (bird.isDead()) {
             return;
         }
         movement();
     }
 
+    // Drawing a moving hover pipe
     private void drawHoverHard(Graphics g) {
         // Number of splices
         int count = (height - 2 * PIPE_HEAD_HEIGHT) / PIPE_HEIGHT + 1;
@@ -66,6 +69,7 @@ public class MovingPipe extends Pipe {
         g.drawImage(pipeImage[1], pipeX - ((PIPE_HEAD_WIDTH - width) /2), y + deltaY, null);
     }
 
+    // Draw the moving water pipe from top to bottom
     private void drawTopHard(Graphics g) {
         // Number of splices
         int count = (height - PIPE_HEAD_HEIGHT) / PIPE_HEIGHT + 1; // Rounding + 1
@@ -78,6 +82,7 @@ public class MovingPipe extends Pipe {
                 height - Constant.TOP_PIPE_LENGTHENING - PIPE_HEAD_HEIGHT + deltaY, null);
     }
 
+    // Draw the moving water pipe from bottom to top
     private void drawBottomHard(Graphics g) {
         // Number of splices
         int count = (height - PIPE_HEAD_HEIGHT) / PIPE_HEIGHT + 1;
@@ -89,6 +94,7 @@ public class MovingPipe extends Pipe {
         g.drawImage(pipeImage[2], pipeX - ((PIPE_HEAD_WIDTH - width)/2), Constant.FRAME_HEIGHT - height + deltaY, null);
     }
 
+    // Movement logic of water pipes
     private void movement() {
         // The motion logic of the pipeX coordinate is the same as that of a normal water pipe
         pipeX -= pipeSpeed;
